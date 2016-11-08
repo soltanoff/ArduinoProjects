c385ce32300a962155a3bef32134725d80639051
Ilya <piccadillable@gmail.com>
2016-11-09 00:38:11 +0300

    added new leds (3 leds to pin #10, 11, 12)

--- SimpleProject/src/main.cpp
+++ SimpleProject/src/main.cpp
@@ -1,14 +1,23 @@
 #include "../lib/blinking.h"
 
 
+int leds[] = {LED_BUILTIN, 10, 11, 12};
+
 void setup()
 {
   TIME = DEFAULT_TIME;
-  pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  for (unsigned int i = 0; i < sizeof(leds)/sizeof(int); i++)
+    pinMode(leds[i], OUTPUT);
 }
 
 void loop()
 {
+  blinkQuery(0, sizeof(leds)/sizeof(int));
+  blinkQuery(sizeof(leds)/sizeof(int) - 1, 0);
+  blinkAll();
+
+  /*
   switch (PROFILE) {
     case 0:
       body(100);
@@ -17,4 +26,5 @@ void loop()
       body(1000, -1);
       break;
   };
+  */
 }
