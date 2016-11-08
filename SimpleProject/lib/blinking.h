f8ef43f21e54b4907347a89759af5779aa4373af
Ilya <piccadillable@gmail.com>
2016-11-09 00:37:17 +0300

    added blinkQuery and blinkAll function.

--- SimpleProject/lib/blinking.h
+++ SimpleProject/lib/blinking.h
@@ -7,7 +7,7 @@ const int DEFAULT_TIME = 6000;
 int TIME;
 short PROFILE = 0;
 /* ========================================================================= */
-void runBlink(int pause)
+void runBlinkMainLed(int pause)
 {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(pause);
@@ -17,12 +17,40 @@ void runBlink(int pause)
 
 void body(int pause, short profileModificator=1)
 {
-  runBlink(pause);
+  runBlinkMainLed(pause);
   TIME -= 2 * pause;
   if (TIME < 0)
   {
     TIME = DEFAULT_TIME;
     PROFILE += profileModificator;
+  }
+}
+
+void blinkQuery(int from, int to, int pause=500)
+{
+  int decr = from < to ? 1: -1;
+  for (int i = from; from < to ? i < to: i >= to; i += decr)
+  {
+    digitalWrite(leds[i], HIGH);
+    delay(pause);
+    digitalWrite(leds[i], LOW);
+    delay(pause);
+  }
+}
+
+void blinkAll(int pause=500)
+{
+  for (unsigned int i = 0; i < sizeof(leds)/sizeof(int); i++)
+  {
+    for (unsigned int j = 0; j < sizeof(leds)/sizeof(int); j++)
+      digitalWrite(leds[j], HIGH);
+
+    delay(pause);
+
+    for (unsigned int j = 0; j < sizeof(leds)/sizeof(int); j++)
+      digitalWrite(leds[j], LOW);
+
+    delay(pause);
   }
 }
 /* ========================================================================= */
