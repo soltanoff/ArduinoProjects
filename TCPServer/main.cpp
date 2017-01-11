c365da5110c750b97332932f34ebe275df3740fb
Ilya <piccadillable@gmail.com>
2017-01-12 02:25:24 +0300

    Create main.cpp

---
+++ TCPServer/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include "lib/server.h"
+
+
+int main()
+{
+    time_t t = time(NULL);
+    tm* aTm = localtime(&t);
+    std::cout
+            << "[START] TIME: "  // %04d/%02d/%02d %02d:%02d:%02d\n",
+            << aTm->tm_year+1900 << "/" << std::setfill ('0') << std::setw (2)
+            << aTm->tm_mon+1 << "/" << std::setfill ('0') << std::setw (2)
+            << aTm->tm_mday << " " << std::setfill ('0') << std::setw (2)
+            << aTm->tm_hour << ":" << std::setfill ('0') << std::setw (2)
+            << aTm->tm_min << ":" << std::setfill ('0') << std::setw (2)
+            << aTm->tm_sec << std::endl;
+
+    Server s;
+    s.start();
+
+    system("pause");
+    return 0;
+}
