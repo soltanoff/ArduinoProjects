4b40056eb2bff1f9c26512544200c6b20e7623fc
Ilya <piccadillable@gmail.com>
2017-01-22 14:55:47 +0300

    Change core files path in include directive for client

--- TCPServer/src/client/client.h
+++ TCPServer/src/client/client.h
@@ -1,9 +1,8 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 /* ================================================================================================================== */
-#include "../config/config.h"
-#include "interaction.h"
-#include "connection.h"
+#include "../core/interaction.h"
+#include "../core/connection.h"
 /* ================================================================================================================== */
 
 
