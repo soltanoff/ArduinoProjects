bc099ba0f30c4aa3028068f5f4d53ee18e3f5e54
Ilya <piccadillable@gmail.com>
2017-01-29 13:02:01 +0300

    Create CDBConnection (database connection instuments)

---
+++ TCPServer [POSIX]/src/core/database/database.h
@@ -0,0 +1,49 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+/* ================================================================================================================== */
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+/* ================================================================================================================== */
+typedef std::vector<std::string> QueryResult;
+typedef std::vector<std::vector<std::string>> QueryResultList;
+
+
+class CDBConnection {
+public:
+    CDBConnection() {};
+    CDBConnection(
+            const std::string server,
+            const std::string database,
+            const std::string user,
+            const std::string password,
+            const unsigned int port = 0
+    );
+    ~CDBConnection() { close(); };
+
+    bool connect(
+            const std::string server,
+            const std::string database,
+            const std::string user,
+            const std::string password,
+            const unsigned int port = 0
+    );
+    void reconnnect();
+    void close();
+
+    MYSQL_RES* query(const char* stmt);
+    QueryResult get_record(const char* stmt);
+    QueryResultList get_record_list(const char* stmt);
+private:
+    MYSQL* _connection;
+
+    unsigned int _port;
+    std::string _server;
+    std::string _database;
+
+    std::string _user;
+    std::string _password;
+};
+#endif /* DATABASE_H */
