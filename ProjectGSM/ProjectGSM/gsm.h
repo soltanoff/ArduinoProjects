1b04b5d5657c6ac8f1240d196e72e17ab5640ca5
Ilya <piccadillable@gmail.com>
2017-01-30 01:30:24 +0300

    Added new field for interact with Server

--- ProjectGSM/ProjectGSM/gsm.h
+++ ProjectGSM/ProjectGSM/gsm.h
@@ -8,7 +8,8 @@
 /* ========================================================================= */
 const short DEFAULT_RX = 10;
 const short DEFAULT_TX = 11;
-const long DEFAULT_SERIAL_PORT = 115200;
+const short DEFUALT_POWER_PIN = 3;
+const long DEFAULT_SERIAL_PORT = 9600;
 /* ========================================================================= */
 class SoftwareGSM
 {
@@ -18,14 +19,23 @@ private:
 	long _serial_port;
 	SoftwareSerial *_gsm_serial;
 	SoftwareSpeaker *_speaker;
+	String _serial_buf;
+
+	bool _is_server_connect;
 public:
 	SoftwareGSM(
 		short rx = DEFAULT_RX,
 		short tx = DEFAULT_TX,
 		long serial_port = DEFAULT_SERIAL_PORT
 	);
+	void send(String &command);
+	
 	void send_sms(const char *phone_number, const char *text);
 	void call_number(const char *phone_number);
+	
+	void connect_to_server(String ip, String port);
+	void disconnect_server();
+
 	void execute();
 };
 /* ========================================================================= */
