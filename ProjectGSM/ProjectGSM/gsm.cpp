2c6573a8bfee02a725704b6b4a079103e6014251
Ilya <piccadillable@gmail.com>
2017-02-03 02:08:41 +0300

    added methods of command interaction

--- ProjectGSM/ProjectGSM/gsm.cpp
+++ ProjectGSM/ProjectGSM/gsm.cpp
@@ -4,21 +4,29 @@
 SoftwareGSM::SoftwareGSM(short rx, short tx, long serial_port)
 {
 	this->_is_server_connect = false;
-	this->_rx_pin = rx;
-	this->_tx_pin = tx;
-	this->_serial_port = serial_port;
+	// this->_rx_pin = rx;
+	// this->_tx_pin = tx;
+	// this->_serial_port = serial_port;
 
-	this->_gsm_serial = new SoftwareSerial(_rx_pin, _tx_pin);
-	this->_gsm_serial->begin(this->_serial_port);  // Скорость порта для связи Arduino с GSM модулем
+	// this->_gsm_serial = new SoftwareSerial(_rx_pin, _tx_pin);
+	// this->_gsm_serial->begin(this->_serial_port);  // Скорость порта для связи Arduino с GSM модулем
 	
+	this->_gsm_serial = new SoftwareSerial(rx, tx);
+	this->_gsm_serial->begin(serial_port);  // Скорость порта для связи Arduino с GSM модулем
+
 	delay(100);
-	this->_gsm_serial->println("AT+IPR=" + String(this->_serial_port));  // устанавливаем скорость шилда
+	this->_gsm_serial->println("AT+IPR=" + String(serial_port));  // устанавливаем скорость шилда
 	delay(100);
 	
 	this->_speaker = new SoftwareSpeaker();
 	this->_speaker->module_initional();
 	delay(1000);
 
+	for (short i = 0; i < 5; i++)
+	{
+		this->_gsm_serial->println("AT");
+		delay(800);
+	}
 	/*
 	Serial.println(this->_rx_pin);
 	Serial.println(this->_tx_pin);
@@ -58,8 +66,92 @@ void SoftwareGSM::call_number(const char *phone_number)
 	this->_gsm_serial->println("ATH");
 }
 /* ========================================================================= */
-void SoftwareGSM::connect_to_server(String ip, String port)
+void SoftwareGSM::prepare_command()
 {
+	_serial_buf += '_';
+	_serial_buf.remove(_serial_buf.length());  // убираем коретку
+	_serial_buf.remove(_serial_buf.length() - 1);  // убираем коретку
+	_serial_buf.remove(_serial_buf.length() - 2);  // убираем коретку
+}
+/* ========================================================================= */
+void SoftwareGSM::prepare_answer(const char* command)
+{
+	// удаляем с ответа саму команду и служебные символы
+	if (_serial_buf.indexOf(command) > -1)
+		_serial_buf.remove(0, strlen(command) + 4); 
+	this->prepare_command();
+}
+/* ========================================================================= */
+bool SoftwareGSM::wait_answer(const char* command)
+{
+	_serial_buf = "";
+	this->_gsm_serial->println(command);
+	Serial.println(command);
+	this->_speaker->serial_sending();
+
+	while (true)
+	{
+		if (this->_gsm_serial->available())
+		{
+			while (this->_gsm_serial->available())
+			{
+				_serial_buf += char(this->_gsm_serial->read());
+				delay(10);
+			}
+			this->_speaker->serial_answear();
+			this->prepare_answer(command);
+			return true;
+		}
+	}
+}
+/* ========================================================================= */
+bool SoftwareGSM::wait_ok_status(const char* command)
+{
+	this->wait_answer(command);
+	Serial.println(_serial_buf);
+	if (_serial_buf.equals("OK"))
+		return true;
+	return false;
+}
+/* ========================================================================= */
+void SoftwareGSM::connect_to_server(const char* ip, const char* port)
+{
+	Serial.println("Connect to server...");
+	
+	this->wait_ok_status("AT+CGATT?");
+	delay(2000);
+	// Прикрепиться сети если 1, 
+	// открепиться если 0 (длительность: 8 сек)
+	if (_serial_buf.indexOf("+CGATT:0") > -1)
+	{
+		this->wait_answer("AT+CGATT=1");  // _gsm_serial->println("AT+CGATT=1");
+		Serial.println(_serial_buf);
+		delay(3000);
+	}
+	// Задаем контекст поключения PDP (Packet Data Protocol)
+	// 1 - индентификатор контекста PDP (max 7)
+	// "IP" - тип протокола (PDP type: IP, IPV6, PPP Point to Point Protocol)
+	// "cmnet" - имя точки доступа
+	this->wait_ok_status("AT+CGDCONT=1,\"IP\",\"cmnet\"");
+	delay(3000);
+	// Активируем заданный контекст (длительность: 3-4 сек)
+	// 1 - состояние (вкл/выкл)
+	// 1 - индентификатор контекста PDP
+	this->wait_ok_status("AT+CGACT=1,1");
+	delay(5000);
+	// Начать TCP/UDP подключение 
+	// (длительность: 2-3 сек)
+	_serial_buf = "AT+CIPSTART=\"TCP\",\"";
+	_serial_buf += ip;
+	_serial_buf += "\", ";
+	_serial_buf += port;
+	this->_gsm_serial->println(_serial_buf.c_str());
+	delay(5000);
+
+	wait_ok_status("AT+CIPSEND=4, \"help\"");
+	this->_is_server_connect = true;
+	delay(1000);
+	/*
 	this->_gsm_serial->println("AT+CGATT=1"); // // Прикрепиться сети если 1, 
 	// открепиться если 0 (длительность: 8 сек)
 	delay(8000);
@@ -81,21 +173,25 @@ void SoftwareGSM::connect_to_server(String ip, String port)
 	this->_gsm_serial->println("AT+CIPSEND=4, \"help\"");
 
 	this->_is_server_connect = true;
+	*/
 }
 /* ========================================================================= */
 void SoftwareGSM::disconnect_server()
 {
-	this->_gsm_serial->println("AT+CIPCLOSE");
+	this->wait_ok_status("AT+CIPCLOSE");
 	this->_is_server_connect = false;
 }
 /* ========================================================================= */
 void SoftwareGSM::send(String &command)
 {
-	if (command.indexOf("dsc") > -1 && this->_is_server_connect)
-		this->disconnect_server();
-	else
-		if (command.indexOf("cnct") > -1)
-			this->connect_to_server("31.207.65.87", "8082");
+	if (command.equals("dsc") && this->_is_server_connect)
+	{
+		this->disconnect_server(); return;
+	}
+	if (command.equals("cnct"))
+	{
+		this->connect_to_server("31.207.65.87", "8082"); return;
+	}
 
 	if (this->_is_server_connect)
 	{
@@ -116,7 +212,7 @@ void SoftwareGSM::execute()
 	{
 		while (this->_gsm_serial->available())
 		{
-			_serial_buf += char(this->_gsm_serial->read());// char(buf);
+			_serial_buf += char(this->_gsm_serial->read());
 			delay(10);
 		}
 		Serial.println(_serial_buf);
@@ -128,10 +224,10 @@ void SoftwareGSM::execute()
 	{
 		while (Serial.available())
 		{
-			_serial_buf += char(Serial.read());// char(buf);
+			_serial_buf += char(Serial.read());
 			delay(10);
 		}
-
+		this->prepare_command();
 		this->send(_serial_buf);
 		this->_speaker->serial_sending();
 	}
--- ProjectGSM/ProjectGSM/gsm.h
+++ ProjectGSM/ProjectGSM/gsm.h
@@ -14,15 +14,21 @@ const long DEFAULT_SERIAL_PORT = 9600;
 class SoftwareGSM
 {
 private:
-	short _rx_pin;
-	short _tx_pin;
-	long _serial_port;
-	SoftwareSerial *_gsm_serial;
+	// short _rx_pin;
+	// short _tx_pin;
+	// long _serial_port;
+	
 	SoftwareSpeaker *_speaker;
 	String _serial_buf;
 
 	bool _is_server_connect;
+	bool wait_ok_status(const char* command);
+	bool wait_answer(const char* command);
+	void prepare_command();
+	void prepare_answer(const char* command);
+	// void read_serial(String &value, SoftwareSerial &serial);
 public:
+	SoftwareSerial *_gsm_serial;
 	SoftwareGSM(
 		short rx = DEFAULT_RX,
 		short tx = DEFAULT_TX,
@@ -33,7 +39,7 @@ public:
 	void send_sms(const char *phone_number, const char *text);
 	void call_number(const char *phone_number);
 	
-	void connect_to_server(String ip, String port);
+	void connect_to_server(const char* ip, const char* port);
 	void disconnect_server();
 
 	void execute();
