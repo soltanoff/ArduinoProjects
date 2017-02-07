a8b95de46fc676d2c585db1f8d9d94f00cb3736f
Ilya <piccadillable@gmail.com>
2017-02-07 09:07:59 +0300

    Change name CBaseInteractionClass -> IBaseInteraction

--- TCPServer [POSIX]/src/core/interaction.h
+++ TCPServer [POSIX]/src/core/interaction.h
@@ -1,12 +1,12 @@
 #ifndef INTERACTION_H
 #define INTERACTION_H
 /* ================================================================================================================== */
-class CBaseInteractionClass {
+class IBaseInteraction{
 public:
     virtual int exec() = 0;
     virtual void close() = 0;
 
-    ~CBaseInteractionClass() = default;
+    virtual ~IBaseInteraction() = default;
 protected:
     virtual bool get_answer(int& bytesRecv, char* answer) = 0;
     virtual bool send_message(const char* msg) = 0;
