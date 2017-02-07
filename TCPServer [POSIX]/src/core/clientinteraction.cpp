4af324dbf3ca6fa1734797dc262209ae5c2b47f0
Ilya <piccadillable@gmail.com>
2017-02-07 09:11:50 +0300

    Set new command scheduler & refactoring

--- TCPServer [POSIX]/src/core/clientinteraction.cpp
+++ TCPServer [POSIX]/src/core/clientinteraction.cpp
@@ -1,10 +1,32 @@
-#include <cstring>
 #include "clientinteraction.h"
-#include "commands/scheduler.h"
 
 
-bool ClientInteraction::get_answer(int& bytesRecv, char* answer) {
-    bytesRecv = recv(_client_socket, answer, ServerCfg::BUFF_SIZE, 0);
+ClientInteraction::ClientInteraction(int client_number, SOCKET client_socket) : // , std::mutex& server_mutex) :
+        _client_number(client_number), _client_socket(client_socket) {
+    // _main_mutex = &server_mutex;
+
+    static auto result_callback = [&](std::vector<std::uint8_t> data) {
+        send_message((const char*)data.data());
+    };
+    static auto shutdown_callback = [&](std::vector<std::uint8_t> data) {
+        send_message((const char*)data.data());
+        throw 1;
+    };
+
+    Scheduler::bind(COMMANDS::unknown, bind(&Functions::unknown, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::help, bind(&Functions::help, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::test, bind(&Functions::test, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::store, bind(&Functions::store, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::get_all, bind(&Functions::get_all, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::get_read, bind(&Functions::get_read, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::get_unread, bind(&Functions::get_unread, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::set_read_all, bind(&Functions::set_read_all, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::clear_db, bind(&Functions::clear_db, _functions), std::ref(result_callback));
+    Scheduler::bind(COMMANDS::shutdown, bind(&Functions::shutdown, _functions), std::ref(shutdown_callback));
+}
+
+bool ClientInteraction::get_answer(std::int32_t& bytesRecv, char* answer) {
+    bytesRecv = (std::int32_t)recv(_client_socket, answer, ServerCfg::BUFF_SIZE, 0);
     if (bytesRecv == 0) {
         std::cout << "[SERVER] Client #" << _client_number + 1 << " disconnected.\n";
         throw 0;
@@ -17,33 +39,48 @@ bool ClientInteraction::get_answer(int& bytesRecv, char* answer) {
 }
 
 bool ClientInteraction::send_message(const char* msg) {
-    MAIN_MUTEX.lock();
-    send(_client_socket, msg, strlen(msg), 0);
-    MAIN_MUTEX.unlock();
+    std::lock_guard<decltype(MAIN_MUTEX)> scoped_lock(MAIN_MUTEX);
+    // std::lock_guard<decltype(*_main_mutex)> scoped_lock(*_main_mutex);
+
+    // std::cout << "send message: " << msg << std::endl;
+    // std::string bytes = std::to_string((unsigned long long)strlen(msg));
+    // send(_client_socket, bytes.data(), bytes.size(), 0);
+    if (!strlen(msg))
+        send(_client_socket, " > NONE", 7, 0);
+    else
+        send(_client_socket, msg, strlen(msg), 0);
     return true;
 }
 
 int ClientInteraction::exec() {
-    int bytesRecv = SOCKET_ERROR;
+    std::int32_t bytesRecv = SOCKET_ERROR;
     char recvbuf[ServerCfg::BUFF_SIZE] = "";
 
     get_answer(bytesRecv, recvbuf);
 
     std::string str;
 
-    MAIN_MUTEX.lock();
-    std::cout
-            << "[Client #" << _client_number + 1
-            << "] Bytes recv: " << bytesRecv << " | [MSG: " << recvbuf << "]\n";
-    MAIN_MUTEX.unlock();
+    {
+        std::lock_guard<decltype(MAIN_MUTEX)> scoped_lock(MAIN_MUTEX);
+        // std::lock_guard<decltype(*_main_mutex)> scoped_lock(*_main_mutex);
+        std::cout
+                << "[Client #" << _client_number + 1
+                << "] Bytes recv: " << bytesRecv << " | [MSG: " << recvbuf << "]\n";
+    }
 
+    if (!Scheduler::schedule(std::vector<std::uint8_t>(recvbuf, recvbuf + bytesRecv))) {
+        std::cout << "[SERVER] Client command is not execute. \n";
+        send_message(" > command is not execute (maybe unknown)");
+    }
+
+    /*
     std::string result = command_scheduler(std::string(recvbuf));
-
     if (result.empty())
         send_message(" ");
     else {
         send_message(result.c_str());
     }
+    */
     return 0;
 }
 
--- TCPServer [POSIX]/src/core/clientinteraction.h
+++ TCPServer [POSIX]/src/core/clientinteraction.h
@@ -2,15 +2,15 @@
 #define SERVERTHREAD_H
 /* ================================================================================================================== */
 #include "../config/config.h"
+#include "../core/commands/scheduler.h"
+#include "../core/commands/functions.h"
 #include "interaction.h"
 /* ================================================================================================================== */
-void thread_routine(int client_number, SOCKET client_socket);
 
 
-class ClientInteraction: public CBaseInteractionClass {
+class ClientInteraction: public IBaseInteraction {
 public:
-    ClientInteraction(int client_number, SOCKET client_socket) :
-            _client_number(client_number), _client_socket(client_socket) {}
+    ClientInteraction(int client_number, SOCKET client_socket);  // , std::mutex& server_mutex);
     ~ClientInteraction() { close(); }
 
     int exec();
@@ -18,9 +18,16 @@ public:
 private:
     int _client_number;
     SOCKET _client_socket;
+    Functions _functions;
+    // std::mutex* _main_mutex;
 
-    bool get_answer(int& bytesRecv, char* answer);
+    bool get_answer(std::int32_t& bytesRecv, char* answer);
     bool send_message(const char* msg);
+
+    template<class TFunc, class TObj>
+    auto bind(TFunc&& fptr, TObj& optr) -> decltype(std::bind(fptr, optr, std::placeholders::_1, std::placeholders::_2)) {
+        return std::bind(fptr, optr, std::placeholders::_1, std::placeholders::_2);
+    }
 };
 /* ================================================================================================================== */
 #endif /* SERVERTHREAD_H */
