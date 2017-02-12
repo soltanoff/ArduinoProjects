aa5933e825c7b1720423c85a17e62e1163c0a89c
Ilya <piccadillable@gmail.com>
2017-02-12 22:59:51 +0300

    Added client socket argument

--- TCPServer [POSIX]/src/core/commands/functions.cpp
+++ TCPServer [POSIX]/src/core/commands/functions.cpp
@@ -16,13 +16,13 @@ std::unique_ptr<CDBConnection> Functions::get_db() {
 // ====================================================================================================================
 // COMMAND FUNCTIONS
 // cmd: None
-void Functions::unknown(DataVector arg, FuncArg result_cb) {
+void Functions::unknown(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str = {" > unknown command"};
-    result_cb(DataVector(str.begin(), str.end() + 1));
+    result_cb(DataVector(str.begin(), str.end() + 1), s);
 }
 
 // cmd: help
-void Functions::help(DataVector arg, FuncArg result_cb) {
+void Functions::help(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str = {"\n\rAvailable commands:\n\r"
                         "> test [{arg}] \t\t - function of the test. Return message.\n\r"
                         "> store {arg} \t\t - method of storing in DB send {args}.\n\r"
@@ -31,11 +31,11 @@ void Functions::help(DataVector arg, FuncArg result_cb) {
                         "> get_unread \t\t - get all unread messages.\n\r"
                         "> set_read_all \t\t - set all messages as read.\n\r"
                         "> clear_db \t\t\t - set all messages as deleted.\n\r"};
-    result_cb(std::vector<std::uint8_t>(str.begin(), str.end() + 1));
+    result_cb(std::vector<std::uint8_t>(str.begin(), str.end() + 1), s);
 }
 
 // cmd: test
-void Functions::test(DataVector arg, FuncArg result_cb) {
+void Functions::test(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
 
     if (str_arg.empty())
@@ -44,11 +44,11 @@ void Functions::test(DataVector arg, FuncArg result_cb) {
         std::cout << "foo() executed. arg = " << str_arg << std::endl;
 
     std::string str = {" > this is a test method"};
-    result_cb(DataVector(str.begin(), str.end() + 1));
+    result_cb(DataVector(str.begin(), str.end() + 1), s);
 }
 
 // cmd: store <args>
-void Functions::store(DataVector arg, FuncArg result_cb) {
+void Functions::store(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
 
     std::string str;
@@ -61,11 +61,11 @@ void Functions::store(DataVector arg, FuncArg result_cb) {
     else
         str = "not enough arguments.";
 
-    result_cb(DataVector(str.begin(), str.end() + 1));
+    result_cb(DataVector(str.begin(), str.end() + 1), s);
 }
 
 // cmd: get_all
-void Functions::get_all(DataVector arg, FuncArg result_cb) {
+void Functions::get_all(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
     std::string result;
 
@@ -85,11 +85,11 @@ void Functions::get_all(DataVector arg, FuncArg result_cb) {
     else
         result = " > getall execute without arguments";
 
-    result_cb(DataVector(result.begin(), result.end() + 1));
+    result_cb(DataVector(result.begin(), result.end() + 1), s);
 }
 
 // cmd: get_read
-void Functions::get_read(DataVector arg, FuncArg result_cb) {
+void Functions::get_read(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
     std::string result;
 
@@ -110,11 +110,11 @@ void Functions::get_read(DataVector arg, FuncArg result_cb) {
     else
         result = " > get_read execute without arguments";
 
-    result_cb(DataVector(result.begin(), result.end() + 1));
+    result_cb(DataVector(result.begin(), result.end() + 1), s);
 }
 
 // cmd: get_unread
-void Functions::get_unread(DataVector arg, FuncArg result_cb) {
+void Functions::get_unread(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
     std::string result;
 
@@ -138,11 +138,11 @@ void Functions::get_unread(DataVector arg, FuncArg result_cb) {
     else
         result = " > get_unread execute without arguments";
 
-    result_cb(DataVector(result.begin(), result.end() + 1));
+    result_cb(DataVector(result.begin(), result.end() + 1), s);
 }
 
 // cmd: set_read_all
-void Functions::set_read_all(DataVector arg, FuncArg result_cb) {
+void Functions::set_read_all(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
     std::string result;
 
@@ -155,11 +155,11 @@ void Functions::set_read_all(DataVector arg, FuncArg result_cb) {
     else
         result = " > clear_db execute without arguments";
 
-    result_cb(DataVector(result.begin(), result.end() + 1));
+    result_cb(DataVector(result.begin(), result.end() + 1), s);
 }
 
 // cmd: clear_db
-void Functions::clear_db(DataVector arg, FuncArg result_cb) {
+void Functions::clear_db(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::string str_arg(arg.begin(), arg.end());
     std::string result;
 
@@ -172,13 +172,13 @@ void Functions::clear_db(DataVector arg, FuncArg result_cb) {
     else
         result = " > clear_db execute without arguments";
 
-    result_cb(DataVector(result.begin(), result.end() + 1));
+    result_cb(DataVector(result.begin(), result.end() + 1), s);
 }
 
 // cmd: shutdown
-void Functions::shutdown(DataVector arg, FuncArg result_cb) {
+void Functions::shutdown(DataVector arg, FuncArg result_cb, SOCKET s) {
     std::cout << "[SERVER] Shutdown... " << std::endl;
 
     std::string str = {" > server is shutdown"};
-    result_cb(DataVector(str.begin(), str.end() + 1));
+    result_cb(DataVector(str.begin(), str.end() + 1), s);
 }
--- TCPServer [POSIX]/src/core/commands/functions.h
+++ TCPServer [POSIX]/src/core/commands/functions.h
@@ -12,29 +12,30 @@ class Functions
 public:
     // ====================================================================================================================
     typedef std::vector<std::uint8_t> DataVector;
-    typedef std::function<void(DataVector)> FuncArg;
+    // typedef std::function<void(DataVector)> FuncArg;
+    typedef std::function<void(DataVector, SOCKET)> FuncArg;
     // ====================================================================================================================
     // COMMAND FUNCTIONS
     // cmd: None
-    void unknown(DataVector arg, FuncArg result_cb);
+    void unknown(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: help
-    void help(DataVector arg, FuncArg result_cb);
+    void help(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: test
-    void test(DataVector arg, FuncArg result_cb);
+    void test(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: store <args>
-    void store(DataVector arg, FuncArg result_cb);
+    void store(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: get_all
-    void get_all(DataVector arg, FuncArg result_cb);
+    void get_all(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: get_read
-    void get_read(DataVector arg, FuncArg result_cb);
+    void get_read(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: get_unread
-    void get_unread(DataVector arg, FuncArg result_cb);
+    void get_unread(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: set_read_all
-    void set_read_all(DataVector arg, FuncArg result_cb);
+    void set_read_all(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: clear_db
-    void clear_db(DataVector arg, FuncArg result_cb);
+    void clear_db(DataVector arg, FuncArg result_cb, SOCKET s);
     // cmd: shutdown
-    void shutdown(DataVector arg, FuncArg result_cb);
+    void shutdown(DataVector arg, FuncArg result_cb, SOCKET s);
 private:
     // ====================================================================================================================
     // SERVICE FUNCTIONS
