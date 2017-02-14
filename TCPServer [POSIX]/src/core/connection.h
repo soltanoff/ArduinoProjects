450e38fa8856210259ed400f577e651c34b58b8d
Ilya <piccadillable@gmail.com>
2017-02-14 10:19:23 +0300

    set virtual destructor of IBaseConnection interface

--- TCPServer [POSIX]/src/core/connection.h
+++ TCPServer [POSIX]/src/core/connection.h
@@ -8,7 +8,7 @@ public:
     virtual int try_open_socket() = 0;
     virtual void start() = 0;
 
-    ~IBaseConnection() = default;
+    virtual ~IBaseConnection() = default;
 protected:
     SOCKET m_socket; // создаем сокет
     sockaddr_in service;
