ce1c0f0de1c74798095b114558177d5a466275f4
Ilya <piccadillable@gmail.com>
2017-02-14 10:14:09 +0300

    clear main.cpp

--- ProjectGSM/src/main.cpp
+++ ProjectGSM/src/main.cpp
@@ -1,17 +1,11 @@
 /* ========================================================================= */
 #include "gsm.h"
-// #include <SoftwareSerial.h>
 /* ========================================================================= */
 SoftwareGSM *gsm;
 /* ========================================================================= */
 void setup()
 {
-	// std::vector<int> f;
-	// //std::function<void(int)> f  = [](int a) { return a + 5; };
-	// int c = 6; //void (*f)(int)
-	// auto f = [&c](int a) { return a + 5; };
-	// bind(int(5), c);
-	Serial.begin(DEFAULT_SERIAL_PORT);  // Скорость порта для связи Arduino с компьютером
+	Serial.begin(DEFAULT_SERIAL_PORT);
 	gsm = new SoftwareGSM();
 	digitalWrite(DEFUALT_POWER_PIN, HIGH);
 	Serial.println("[GSM] module started.");
