fe25cf308f0860ad76815bc3432cedb9db7f3c08
Ilya <piccadillable@gmail.com>
2017-02-15 10:44:55 +0300

    remove logic of server client

--- TCPServer [POSIX]/src/core/commands/scheduler.cpp
+++ TCPServer [POSIX]/src/core/commands/scheduler.cpp
@@ -15,7 +15,6 @@ bool Scheduler::execute_command(const std::uint32_t& cmd_code, const std::string
 }
 
 int Scheduler::schedule(const std::vector<uint8_t> &data, SOCKET s, const bool& is_gsm) {
-    static std::regex re("(\\w+)\\s*(.*)?\\s*");
     /*
     struct S
     {
@@ -40,38 +39,15 @@ int Scheduler::schedule(const std::vector<uint8_t> &data, SOCKET s, const bool& 
     try
     {
         std::string command(data.begin(), data.end());
-
         if (command == "close") {
             return -1;
         }
 
         if (not is_gsm) {
-            std::smatch result;
-            if (std::regex_search(command, result, re)) {
-                std::string cmd(result.str(1));
-                std::string args = result.str(2);
-                // ============================ ==============
-                // Command selector
-                COMMANDS cmd_code;
-                if (cmd == "help")
-                    cmd_code = COMMANDS::help;
-                else if (cmd == "store")
-                    cmd_code = COMMANDS::store;
-                else if (cmd == "get_all")
-                    cmd_code = COMMANDS::get_all;
-                else if (cmd == "get_read")
-                    cmd_code = COMMANDS::get_read;
-                else if (cmd == "get_unread")
-                    cmd_code = COMMANDS::get_unread;
-                else if (cmd == "set_read_all")
-                    cmd_code = COMMANDS::set_read_all;
-                else if (cmd == "clear_db")
-                    cmd_code = COMMANDS::clear_db;
-                else
-                    cmd_code = COMMANDS::unknown;
+            COMMANDS  cmd_code = (COMMANDS) std::stoi(command.substr(0, 2));
+            std::string args = command.substr(3, command.length());
 
-                return Scheduler::execute_command((std::uint32_t) cmd_code, args, s);
-            } else return 0;
+            return Scheduler::execute_command((std::uint32_t) cmd_code, args, s);
         }
         else {
             return Scheduler::execute_command((std::uint32_t) COMMANDS::store, command, s);
@@ -90,3 +66,4 @@ void Scheduler::bind(COMMANDS cmd, Func&& callback, ResultCallback&& result_cb) 
 }
 
 std::map<std::uint32_t, std::list<std::pair<Scheduler::Func, Scheduler::ResultCallback>> > Scheduler::_callbacks_map = {};
+ `
