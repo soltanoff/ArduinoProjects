a339863d1fd87619013eea40c03a67482d1c5df2
Ilya <piccadillable@gmail.com>
2017-03-21 22:22:55 +0300

    refactoring SoftwareSpeaker

--- ProjectGSM/src/speaker.h
+++ ProjectGSM/src/speaker.h
@@ -6,15 +6,15 @@
 // ============================================================================
 class SoftwareSpeaker
 {
-private:
-	short _pin;
-	void speaker_voice(int time = 500, int iteration = 1, int frequency = 0);
 public:
 	SoftwareSpeaker(short pin = 2);
 	void module_initional();
 	// void sms_sending();
 	void serial_sending();
 	void serial_answer();
+private:
+	short _pin;
+	void speaker_voice(int time = 500, int iteration = 1, int frequency = 0);
 };
 // ============================================================================
 #endif /* SPEAKER_H */
