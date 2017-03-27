d2d24da00d1da75fba3940c40353ca4fe63a9d44
Ilya <piccadillable@gmail.com>
2017-03-27 10:16:41 +0300

    Create Traffic class. This is class of usb-traffic analyzer

---
+++ ProjectGSM/src/includes/traffic/traffic.h
@@ -0,0 +1,17 @@
+// ============================================================================
+#ifndef TRAFFIC_H
+#define TRAFFIC_H
+// ============================================================================
+#include "./includes/device/gsm/gsm.h"
+#include "./includes/device/keyboard/keyboard.h"
+// ============================================================================
+const uint8_t MAX_BUFFER_SIZE = 10;
+
+class Traffic {
+public:
+    static void send(SoftwareGSM* gsm_module, KeyboardParser* keyboard_prs);
+    static void tanalyze(SoftwareGSM* gsm_module, KeyboardParser* keyboard_prs);
+};
+// ============================================================================
+#endif /* TRAFFIC_H */
+// ============================================================================
