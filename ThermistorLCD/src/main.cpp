903f6244bf564333baf77147617f71c739238094
soltanoff <piccadillable@gmail.com>
2020-08-01 22:06:28 +0300

    added logic impl

--- ThermistorLCD/src/main.cpp
+++ ThermistorLCD/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+const byte rs = 8, en = 9, d4 = 10, d5 = 11, d6 = 12, d7 = 13;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const byte THER_PIN = 0;
+
+int counter = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:");
+  lcd.setCursor(0, 1);
+  lcd.print("Time:");
+
+  pinMode(THER_PIN, INPUT);
+}
+
+void processLCD() {
+  lcd.setCursor(6, 1);
+  counter++;
+  if (counter == 10) { counter = 0; };
+  lcd.print(counter);
+}
+
+void processThermister() {
+  int data = analogRead(THER_PIN);
+  double t = 4090 / (log((1025.0 * 10 / data - 10) / 10) + 4090 / 298.0) - 273.0;
+  
+  lcd.setCursor(6, 0);
+  lcd.print(t);
+  lcd.print(" C");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(500);
+  processLCD();
+  processThermister();
 }
